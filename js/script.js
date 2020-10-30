5d4c287e2f03a0e37a6194a4e4f80699398f210f
Kevin Wilson Reaño Cisneros <kreanoci@everis.com>
2020-10-30 01:42:39 -0500

    Se ha agregado el logo

--- js/script.js
+++ js/script.js
@@ -5,17 +5,22 @@
     if(menuButton && menuNav){
         menuButton.addEventListener('click', (e)=>{
             e.preventDefault();
+            menuButton.classList.toggle('open')
             menuNav.classList.toggle('open');
         })
     }
 })(document);
 
 (function (d) {
-    const mainMenu = d.getElementById("main-menu");
+    const mainMenu = d.getElementById("menu");
     if (mainMenu) {
         mainMenu.addEventListener("click", (e) => {
             e.preventDefault();
-            e.target.parentNode.classList.toggle("selected");
+            let element = e.target;
+            if (element.classList.contains('menu__link')){
+                e.target.parentNode.classList.toggle("selected");
+            }
+            // console.log(e.target.getAttribute('aria-expanded'))
         });
     }
 })(document);
